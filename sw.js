8d6bd2f84d0cb4251fb2098da0318e476afb419a
Roba <75736399+Robel43@users.noreply.github.com>
2026-09-25 11:57:45 +0300

    Bump PWA cache for breakdown update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'birrtrack-v10';
+const CACHE = 'birrtrack-v11';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest',
   './icons/birrtrack-192-v2.png', './icons/birrtrack-512-v2.png'
